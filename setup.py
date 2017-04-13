40db8fcfeda08f284efbcfdc39ba6b4783fa9690
Helmut Merz <helmutm@cy55.de>
2017-04-13 15:55:28 +0200

    initial import

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.md').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='ccq.misc',
+      version=version,
+      description="miscellaneous customizations",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='https://www.cyberconcepts.org',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['ccq'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ src/ccq/misc/__init__.py
@@ -0,0 +1,13 @@
+from cco.skin.r2 import CCO_R2
+
+
+class CCQ_Common(CCO_R2):
+    """A parent class for similar skins."""
+
+
+class CCQ_FCO(CCQ_Common):
+    """The CCQ_FCO (functionalconcepts.org) skin."""
+
+
+class CCQ_KMatrix(CCQ_Common):
+    """The CCQ_KMatrix (k-matrix.org) skin."""
